67b493be0c5889f2c2763d7d5fc148ca9ebf7897
Henri Cavalheiro <167575522+livia19gomes@users.noreply.github.com>
2024-11-29 07:49:36 -0300

    .

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, flash, request, session
+,3from flask import Flask, render_template, redirect, url_for, flash, request, session
 import fdb
 from datetime import datetime
 
